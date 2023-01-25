49110a0e933c1191f65fa5ab09ea11c5bf09221b
Artur Chemov <arthur.chemov@gmail.com>
2023-01-26 01:05:19 +0200

    Update layout

--- src/App.js
+++ src/App.js
@@ -3,7 +3,12 @@ import "./scss/custom.scss"
 function App() {
   return (
     <div className="app bg-gradient">
-      <header className="container text-white pt-2 h2">Weather <i className="bi bi-thermometer-sun"></i></header>
+      <header className="container text-white pt-2 h2">
+        Weather<i className="bi bi-thermometer-sun"></i>
+      </header>
+      <main className="container d-inline-flex p-2 bd-highlight justify-content-end">
+        <i class="bi bi-emoji-sunglasses"></i>
+      </main>
     </div>
   );
 }
